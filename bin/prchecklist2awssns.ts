e597faca501d71a9899b0523e3f619b0f6390b41
Windymelt <windymelt@hatena.ne.jp>
2019-12-19 15:53:17 +0900

    eslint

--- bin/prchecklist2awssns.ts
+++ bin/prchecklist2awssns.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import cdk = require('@aws-cdk/core');
-import { Prchecklist2AwssnsStack } from '../lib/prchecklist2awssns-stack';
+import "source-map-support/register";
+import cdk = require("@aws-cdk/core");
+import { Prchecklist2AwssnsStack } from "../lib/prchecklist2awssns-stack";
 
 const app = new cdk.App();
-new Prchecklist2AwssnsStack(app, 'Prchecklist2AwssnsStack');
+new Prchecklist2AwssnsStack(app, "Prchecklist2AwssnsStack");
--- lib/prchecklist2awssns-stack.ts
+++ lib/prchecklist2awssns-stack.ts
@@ -1,9 +1,11 @@
-import cdk = require('@aws-cdk/core');
+import cdk = require("@aws-cdk/core");
 
 export class Prchecklist2AwssnsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // TODO: API Gateway
+    // TODO: SNS
+    // TODO: Lambda
   }
 }
